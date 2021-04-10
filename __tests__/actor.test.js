abc2c9892797707a504fc9899ba3c668b5e630c0
zhaned <zhanedmond@gmail.com>
2021-04-09 17:40:30 -0700

    working on actor stuff

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -8,4 +8,60 @@ describe('Actor routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
+
+  it('get all actors', async() => {
+    const actor = await Actor.create({
+      name: 'John Doe',
+      dob: '1980-06-17',
+      pob: 'place of birth'
+    });
+
+    return request(app)
+    .get('/api/v1/actors')
+    .then((res) => {
+      expect(res.body).toEqual([{
+        id: 1,
+        name: 'John Doe',
+        dob: '1980-06-17',
+        pob: 'place of birth'
+      }]);
+    })
+  })
+
+  it('post a new actor', async() => {
+    return request(app)
+    .post(`/api/v1/actors`)
+    .send({
+      name: 'Jane Doe',
+      dob: '1980-06-17',
+      pob: 'place of birth'
+    })
+    .then((res) => {
+      expect(res.body).toEqual({
+        id: 1,
+        name: 'Jane Doe',
+        dob: '1980-06-17',
+        pob: 'place of birth'
+      });
+    })
+  })
+
+  it('get an actor by id', async() => {
+    const actor = await Actor.create({
+      name: 'John Doe',
+      dob: '1980-06-17',
+      pob: 'place of birth',
+    })
+    return request(app)
+    .get(`/api/v1/actors/${actor.id}`)
+    .then((res) => {
+      expect(res.body).toEqual({
+        id: 1,
+        name: 'John Doe',
+        dob: '1980-06-17',
+        pob: 'place of birth',
+      });
+    })
+  })
+
 });
