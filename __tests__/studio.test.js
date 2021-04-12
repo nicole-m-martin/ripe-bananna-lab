d4ed8a588dc204fca2b0851f4e59e951cde3dd81
Brandon Perard <brandon.perard@gmail>
2021-04-12 10:54:41 -0700

    add Studio model, controller, and initial tests for vertical build

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -8,4 +8,74 @@ describe('Studio routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
+
+  it('get all studios', async () => {
+    const studio = await Studio.create({
+      name: 'John Doe',
+      city: 'Death Valley',
+      state: 'CA',
+      country: 'USA'
+    });
+
+    const studio1 = await Studio.create({
+      name: 'Jane Doe',
+      city: 'Death Valley',
+      state: 'CA',
+      country: 'USA'
+    });
+
+    console.log('RIGHT HERE', studio);
+    return request(app)
+      .get('/api/v1/studios')
+      .then((res) => {
+        expect(res.body).toEqual([{
+          id: 1,
+          name: 'John Doe',
+        },
+        {
+          name: 'Jane Doe',
+          id: 2
+        }]);
+      })
+  })
+
+  it('post a new studio', async () => {
+    return request(app)
+      .post(`/api/v1/studios`)
+      .send({
+        name: 'Jane Doe',
+        city: 'Death Valley',
+        state: 'CA',
+        country: 'USA'
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Jane Doe',
+          city: 'Death Valley',
+          state: 'CA',
+          country: 'USA'
+        });
+      })
+  })
+
+  it('get an studio by id', async () => {
+    const studio = await Studio.create({
+      name: 'Jane Doe',
+      city: 'Death Valley',
+      state: 'CA',
+      country: 'USA'
+    })
+    return request(app)
+      .get(`/api/v1/studios/${studio.id}`)
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Jane Doe',
+          city: 'Death Valley',
+          state: 'CA',
+          country: 'USA'
+        });
+      })
+  })
 });
