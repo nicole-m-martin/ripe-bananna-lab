d52db79ca8bff064bc84566ef4f2537aae566fe6
Brandon Perard <brandon.perard@gmail>
2021-04-12 11:17:24 -0700

    add Reviewer model, controller, and initial tests for vertical build

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -8,4 +8,83 @@ describe('Reviewer routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
+
+  it('get all reviewers', async () => {
+    const reviewer = await Reviewer.create({
+      name: 'John Doe',
+      company: 'Ripe Bananna'
+
+    });
+
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then((res) => {
+        expect(res.body).toEqual([{
+          id: 1,
+          name: 'John Doe',
+          company: 'Ripe Bananna'
+        }]);
+      })
+  })
+
+  it('post a new reviewer', async () => {
+    return request(app)
+      .post(`/api/v1/reviewers`)
+      .send({
+        name: 'John Doe',
+        company: 'Ripe Bananna'
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'John Doe',
+          company: 'Ripe Bananna'
+        });
+      })
+  })
+
+  it('get an reviewer by id', async () => {
+    const reviewer = await Reviewer.create({
+      name: 'John Doe',
+      company: 'Ripe Bananna'
+    })
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer.id}`)
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'John Doe',
+          company: 'Ripe Bananna'
+        });
+      })
+  })
+
+  it('update a reviewer by id', async () => {
+    const reviewer = await Reviewer.create({
+      name: 'John Doe',
+      company: 'Ripe Bananna'
+    })
+    return request(app)
+      .patch(`/api/v1/reviewers/${reviewer.id}`)
+      .send({ name: 'Jane Doe' })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Jane Doe',
+          company: 'Ripe Bananna'
+        });
+      })
+  })
+
+  it('get an reviewer by id', async () => {
+    const reviewer = await Reviewer.create({
+      name: 'John Doe',
+      company: 'Ripe Bananna'
+    })
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewer.id}`)
+      .then((res) => {
+        expect(res.body).toEqual({ success: '👍' });
+      })
+  })
 });
